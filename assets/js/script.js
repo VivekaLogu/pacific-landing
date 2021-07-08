50c8245dfed2e87e329709c670962057486f6c25
paunkumar <paunvini@gmail.com>
2021-07-08 18:36:58 +0530

    ui changes, add blog content and links

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,15 @@ $(document).ready(function() {
             }, false)
         })
 })()
+$(document).ready(function(){
+
+    $('a[href^="#"]').on('click',function (e) {
+        var target = this.hash
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 0
+        }, 1000, 'swing', function () {
+            window.location.hash = target;   
+        });
+    });
+}); 
